c1332f59014077184368634ff4394cfc400bc3a0
Gabriel Viana <viannadoze@gmail.com>
2025-08-21 11:28:55 -0300

    test: Testing transaction creation

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,7 +1,22 @@
-import { expect, test } from 'vitest'
+import { test, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
 
-test('o usuário consegue criar uma nova transação', () => {
-  const responseStatusCode = 201
+beforeAll(async () => {
+  await app.ready()
+})
 
-  expect(responseStatusCode).toEqual(201)
+afterAll(async () => {
+  await app.close()
+})
+
+test('user can create a new transaction', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New Transactions',
+      amount: 5000,
+      type: 'credit',
+    })
+    .expect(201)
 })
